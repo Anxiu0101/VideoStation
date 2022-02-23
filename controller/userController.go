efb2228d0908c38a166874f595aa48d5d186282e
Anxiu <3387623168@qq.com>
2022-02-23 15:39:17 +0800

    [feat] swagger and dockerfile set

--- controller/userController.go
+++ controller/userController.go
@@ -2,6 +2,12 @@ package controller
 
 import "github.com/gin-gonic/gin"
 
+// GetUserInfo
+// @Summary 获取用户详细信息
+// @Produce  json
+// @Param token query string true "token"
+// @Success 200 {string} json "{"code":200,"data":{},"msg":"ok"}"
+// @Router /user/space [get]
 func GetUserInfo(c *gin.Context) {
 	//username = c.Query("username")
 	//password = c.Query("password")
--- routers/router.go
+++ routers/router.go
@@ -3,10 +3,14 @@ package routers
 import (
 	"VideoStation/controller"
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func InitRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	userApi := r.Group("/")
 	{
