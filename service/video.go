2eea63db0517490f736b52b3b64793aa785c02bb
Anxiu <3387623168@qq.com>
2022-03-08 21:02:34 +0800

    [fix] ShowVideos struct

--- service/video.go
+++ service/video.go
@@ -1,4 +1,8 @@
 package service
 
 type ShowVideos struct {
+	UpID         uint   `json:"publisher_id"`
+	Title        string `json:"title" gorm:"size:50;index"`
+	Video        string `json:"video" gorm:"size:255"`
+	Introduction string `json:"introduction" gorm:"size:255"`
 }
